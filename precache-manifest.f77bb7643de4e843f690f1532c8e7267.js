e58c704a1b6ceed59d3ffb264ebf5780f1ea2858
ricsc <ddwrtkong@t-online.de>
2023-06-16 22:48:51 +0200

    Updates

---
+++ precache-manifest.f77bb7643de4e843f690f1532c8e7267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2346a428225418bb7988797371f5951",
+    "url": "/skinscreen/index.html"
+  },
+  {
+    "revision": "71aa486f1907c50ac527",
+    "url": "/skinscreen/static/css/2.58337e42.chunk.css"
+  },
+  {
+    "revision": "d3b85e3e2ca0ab3788be",
+    "url": "/skinscreen/static/css/main.8e6300ec.chunk.css"
+  },
+  {
+    "revision": "71aa486f1907c50ac527",
+    "url": "/skinscreen/static/js/2.46b35099.chunk.js"
+  },
+  {
+    "revision": "d3b85e3e2ca0ab3788be",
+    "url": "/skinscreen/static/js/main.12316395.chunk.js"
+  },
+  {
+    "revision": "d9d364ee8534f69d43af",
+    "url": "/skinscreen/static/js/runtime~main.8c1d41bd.js"
+  }
+]);
