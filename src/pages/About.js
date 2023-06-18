ba3a52d1a9001b6ccefd6e3a99f4fffda52f0c2a
ricsc <ddwrtkong@t-online.de>
2023-06-18 11:53:44 +0200

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,9 +13,11 @@ export default class Home extends Component {
         <p>
           This is a TensorFlow.js web application where users can classify images selected locally
           or taken with their device's camera. The app uses TensorFlow.js and a pre-trained model
-          converted to the TensorFlow.js format to provide the inference capabilities.
+          converted to the TensorFlow.js format to provide the inference capabilities. This specific model
+          was trained to classify moles e.g. for skin cancer. HAM10000 data was used for training.
           This model is saved locally in the browser using IndexedDB. A service worker is also used
-          to provide offline capabilities.
+          to provide offline capabilities. This app is not supposed to replace a dermatologist, but may give
+          you a hint if you should seek a doctor imediately. Use of the model in commercial environments is prohibited.
         </p>
       </div>
     );
